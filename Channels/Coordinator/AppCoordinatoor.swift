97ce14b1671cac77b0d75b65564c4271ed9ae7c4
Mai Hassen <mai.hassen@ibtikar.net.sa>
2020-03-24 00:04:33 +0200

    App Structure Work Done : - Add Coordinator Layer Classes (Coordinator and AppCoordinator)

---
+++ Channels/Coordinator/AppCoordinatoor.swift
@@ -0,0 +1,49 @@
+//
+//  AppCoordinatoor.swift
+//  Channels
+//
+//  Created by Mai hassen on 3/23/20.
+//  Copyright © 2020 Mai hassen. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ApplicationCoordinator: Coordinator {
+    
+    var window: UIWindow?
+    var rootCoordinator: Coordinator?
+    var appRouter: AppRouter
+    var navigationController: UINavigationController
+
+    override init() {
+        self.navigationController = UINavigationController()
+        
+        self.appRouter = AppRouter(navigationController: navigationController)
+    }
+    
+    override func start() {
+
+        if #available(iOS 13, *) {
+            let scene = UIApplication.shared.connectedScenes.first
+            guard let windowScene = (scene as? UIWindowScene) else { return }
+            if window == nil {
+            window = UIWindow(windowScene: windowScene)
+            }
+        } else {
+            guard let appwindow = (UIApplication.shared.delegate?.window as? UIWindow) else { return }
+            window = appwindow
+        }
+        self.startHome()
+        window?.rootViewController = appRouter.toPresentable()
+        window?.makeKeyAndVisible()
+        
+    }
+    func startHome() {
+        let homeCoordinator = HomeCoordinator(appRouter: appRouter)
+        self.rootCoordinator = homeCoordinator
+        self.rootCoordinator?.start()
+        
+    }
+    
+}
