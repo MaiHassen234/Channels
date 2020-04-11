c57b548576d09d1228184108cb27a9ff8a9b784b
Mai Hassen <mai.hassen@ibtikar.net.sa>
2020-04-11 18:26:34 +0200

    Splash Work Done : - clean code and read images from swiftgen

---
+++ Channels/Constants/Utilities.swift
@@ -0,0 +1,16 @@
+//
+//  Utilities.swift
+//  Channels
+//
+//  Created by Mai hassen on 4/10/20.
+//  Copyright © 2020 Mai hassen. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Utilities {
+   static var screenWidth = UIScreen.main.bounds.width
+   static var screenHeight = UIScreen.main.bounds.height
+}
+
--- Channels/Scenes/Splash/SplashViewController.swift
+++ Channels/Scenes/Splash/SplashViewController.swift
@@ -9,82 +9,80 @@
 import UIKit
 
 class SplashViewController: UIViewController {
+    // MARK: - IBOutlets
     @IBOutlet private weak var channelsImageView: UIImageView!
+    // MARK: - Lazy Variables
+    lazy var leftSplashImage: UIImageView = {
+
+        let imageView = UIImageView(image: Asset.Images.leftSplash.image)
+        imageView.frame = CGRect(x: 0, y: 0, width: 154, height: 128)
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+
+    }()
     
+    lazy var rightSplashImage: UIImageView = {
+
+        let imageView = UIImageView(image: Asset.Images.rightSplash.image)
+        imageView.frame = CGRect(x: Utilities.screenWidth, y: Utilities.screenHeight,
+                                 width: 154, height: 128)
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+
+    }()
+  // MARK: - ViewController Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         // Do any additional setup after loading the view.
-        startAnimation()
+        setupView()
     }
     
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        startAnimation()
         
+    }
+    
 }
 
 // MARK: - Private Methods
 
 extension SplashViewController {
-
-    private func startAnimation() {
-          let screenWidth = UIScreen.main.bounds.width
-          let screenHeight = UIScreen.main.bounds.height
-          
-          let leftSplashImage = UIImageView(frame: CGRect(x: 0,
-                                                          y: 0,
-                                                          width: 154,
-                                                          height: 128))
-          
-          leftSplashImage.image = UIImage(named: "LeftSplash")
-          
-          self.view.addSubview(leftSplashImage)
-          
-          let leftSplashImageXOffset = screenWidth / 2 - leftSplashImage.frame.width + 20
-          
-          let leftSplashImageYOffset = screenHeight / 2 - leftSplashImage.frame.height
-                  
-          let rightSplashImage = UIImageView(frame: CGRect(x: screenWidth,
-                                                           y: screenHeight,
-                                                           width: 154,
-                                                           height: 128))
-
-          rightSplashImage.image = UIImage(named: "RightSplash")
-          
-          self.view.addSubview(rightSplashImage)
-          
-          let rightSplashImageXOffset = -screenWidth / 2 - rightSplashImage.frame.width / 2 + 20
-           
-          let rightSplashImageYOffset = -screenHeight / 2 - rightSplashImage.frame.height
+    private func setupView() {
         
-          let animator = UIViewPropertyAnimator(duration: 4, curve: .linear) {
-              leftSplashImage.frame = leftSplashImage.frame.offsetBy(dx: leftSplashImageXOffset,
-                                                                     dy: leftSplashImageYOffset)
-              rightSplashImage.frame = rightSplashImage.frame.offsetBy(dx: rightSplashImageXOffset,
-                                                                       dy: rightSplashImageYOffset)
-              
-          }
-          
-          animator.startAnimation()
-          
-          UIView.animateKeyframes(withDuration: 4.0, delay: 0.0, options: [], animations: {
-              UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 1.0, animations: {
-                 
-                  self.channelsImageView.transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
-                  self.channelsImageView.alpha = 0
-              })
-              UIView.addKeyframe(withRelativeStartTime: 3.0, relativeDuration: 1, animations: {
-              
-                  self.channelsImageView.transform = CGAffineTransform(scaleX: 1, y: 1)
-                  self.channelsImageView.alpha = 1
-                  })
-              })
+        self.view.addSubview(leftSplashImage)
+        
+        self.view.addSubview(rightSplashImage)
+        
+    }
+        
+    private func startAnimation() {
+        
+        let leftSplashImageXOffset = Utilities.screenWidth / 2 - leftSplashImage.frame.width + 20
+        let leftSplashImageYOffset = Utilities.screenHeight / 2 - leftSplashImage.frame.height
+        let rightSplashImageXOffset = -Utilities.screenWidth / 2 - rightSplashImage.frame.width / 2 + 20
+        let rightSplashImageYOffset = -Utilities.screenHeight / 2 - rightSplashImage.frame.height
+                
+        let animator = UIViewPropertyAnimator(duration: 4, curve: .linear) {
+            self.leftSplashImage.frame = self.leftSplashImage.frame.offsetBy(dx: leftSplashImageXOffset,
+                                                                             dy: leftSplashImageYOffset)
+            self.rightSplashImage.frame = self.rightSplashImage.frame.offsetBy(dx: rightSplashImageXOffset,
+                                                                               dy: rightSplashImageYOffset)
+        }
+        
+        animator.startAnimation()
+        
+        UIView.animateKeyframes(withDuration: 4.0, delay: 0.0, options: [], animations: {
+            UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 1.0, animations: {
+                
+                self.channelsImageView.transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
+                self.channelsImageView.alpha = 0
+            })
+            UIView.addKeyframe(withRelativeStartTime: 3.0, relativeDuration: 1, animations: {
+                
+                self.channelsImageView.transform = CGAffineTransform(scaleX: 1, y: 1)
+                self.channelsImageView.alpha = 1
+            })
+        })
     }
 }
